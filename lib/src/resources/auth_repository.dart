14d7172e3a129718da7ce8e5ea55aae401610b49
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-01-25 17:55:37 +0500

    Added shared prefs

--- lib/src/resources/auth_repository.dart
+++ lib/src/resources/auth_repository.dart
@@ -1,5 +1,6 @@
 import 'package:butler_app/src/models/auth_credentials.dart';
 import 'package:butler_app/src/resources/services/auth_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum CredentialType {
   Email,
@@ -27,6 +28,8 @@ class AuthRepository {
 
       final currentUser = await _auth.getCurrentUser();
 
+      if (currentUser != null) await _setAuthStatus(true);
+
       return currentUser != null ? true : false;
     } catch (_) {
       return false;
@@ -41,6 +44,24 @@ class AuthRepository {
 
     final currentUser = await _auth.getCurrentUser();
 
+    if (currentUser != null) await _setAuthStatus(true);
+
     return currentUser != null ? true : false;
+  }
+
+  Future<bool> checkAuthStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final flag = prefs.getBool('authenticated');
+
+    return flag ?? false;
+  }
+
+  Future<void> _setAuthStatus(bool status) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    try {
+      await prefs.setBool('authenticated', status);
+    } catch (_) {}
   }
 }
