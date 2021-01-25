d3c641e06e75702c79a98e0c3927e73221eddce3
ihassanjavaid <great.maliks@gmail.com>
2021-01-25 12:11:24 +0500

    psh

---
+++ lib/src/resources/utilities/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 24.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 16.0,
+);
+
+const kBackgroundColor = Color(0xff0A1529);
+const kDullBlueColor = Color(0xff222C3E);
+
+const kDefaultIconColour = Color(0xffdadada);
+const kSelectedIconColour = Color(0xFFDF4E67);
