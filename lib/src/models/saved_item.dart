7b43d1cbdedd668a426e91bdd146170e5626fcff
ihassanjavaid <great.maliks@gmail.com>
2021-01-25 12:56:53 +0500

    updoot

--- lib/src/models/saved_item.dart
+++ lib/src/models/saved_item.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class SavedItem{
-  String name;
+abstract class SavedItem{
+  String title;
   String type;
-  String by;
   String language;
+  String genre1;
+  String genre2;
+  String releaseDate;
 
-  SavedItem({this.name, @required this.type, this.by = 'null', this.language});
+  SavedItem({this.title, @required this.type, this.language = 'English', this.genre1 = 'null', this.genre2 = 'null', this.releaseDate});
+}
+
+class SavedMovie extends SavedItem{
+  String popularity;
+
+  SavedMovie({title, language, this.popularity, genre1, genre2, relD}){
+    super.type = 'Movie';
+    super.title = title;
+    super.genre1 = genre1;
+    super.genre2 = genre2;
+    super.releaseDate = relD;
+  }
+}
+
+class SavedBook extends SavedItem{
+  String author = 'null';
+
+  SavedBook({title, language, genre1, genre2, relD, this.author}){
+    super.type = 'Book';
+    super.title = title;
+    super.genre1 = genre1;
+    super.genre2 = genre2;
+    super.releaseDate = relD;
+  }
+}
+
+class SavedPodcast extends SavedItem{
+
+  SavedPodcast({title, language, genre1, genre2, relD}){
+    super.type = 'Podcast';
+    super.title = title;
+    super.genre1 = genre1;
+    super.genre2 = genre2;
+    super.releaseDate = relD;
+  }
+}
+
+class SavedTVShow extends SavedItem{
+
+  SavedTVShow({title, language, genre1, genre2, relD}){
+    super.type = 'TV Show';
+    super.title = title;
+    super.genre1 = genre1;
+    super.genre2 = genre2;
+    super.releaseDate = relD;
+  }
+}
+
+class SavedGame extends SavedItem{
+
+  SavedGame({title, language, genre1, genre2, relD}){
+    super.type = 'Game';
+    super.title = title;
+    super.genre1 = genre1;
+    super.genre2 = genre2;
+    super.releaseDate = relD;
+  }
 }
--- lib/src/resources/services/firestore_service.dart
+++ lib/src/resources/services/firestore_service.dart
@@ -13,20 +13,66 @@ class FirestoreService{
     return _user.email;
   }
 
-  Future<void> postItemOnFirestore(SavedItem item) async{
+  Future<List<QuerySnapshot>> getUsersSavedItems() async {
+    await _auth.checkInternConnection();
+    final _email = await getCurrentUserEmail();
+
+    List<QuerySnapshot> snapshots = List.empty(growable: true);
+
+    QuerySnapshot querySnapshotMovies = await _firestore
+        .collection('movies')
+        .where('email', isEqualTo: _email)
+        .orderBy('timestamp', descending: true)
+        .get();
+    snapshots.add(querySnapshotMovies);
+
+    QuerySnapshot querySnapshotBooks = await _firestore
+        .collection('books')
+        .where('email', isEqualTo: _email)
+        .orderBy('timestamp', descending: true)
+        .get();
+    snapshots.add(querySnapshotBooks);
+
+    QuerySnapshot querySnapshotTvShows = await _firestore
+        .collection('tv_shows')
+        .where('email', isEqualTo: _email)
+        .orderBy('timestamp', descending: true)
+        .get();
+    snapshots.add(querySnapshotTvShows);
+
+    QuerySnapshot querySnapshotGames = await _firestore
+        .collection('games')
+        .where('email', isEqualTo: _email)
+        .orderBy('timestamp', descending: true)
+        .get();
+    snapshots.add(querySnapshotGames);
+
+    QuerySnapshot querySnapshotPodcasts = await _firestore
+        .collection('podcasts')
+        .where('email', isEqualTo: _email)
+        .orderBy('timestamp', descending: true)
+        .get();
+    snapshots.add(querySnapshotPodcasts);
+
+    return snapshots;
+  }
+
+  Future<void> postMovieonFirestore(SavedMovie savedMovie) async {
     await _auth.checkInternConnection();
     final _email = await getCurrentUserEmail();
 
     DocumentReference _ref =
-    _firestore.collection('saved').doc();
+    _firestore.collection('movies').doc();
 
     try{
       await _ref.set({
         'email': _email,
-        'title': item.name,
-        'by': item.by,
-        'type': item.type,
-        'lang': item.language,
+        'title': savedMovie.title,
+        'lang': savedMovie.language,
+        'popularity': savedMovie.popularity,
+        'genre1': savedMovie.genre1,
+        'genre2': savedMovie.genre2,
+        'relDate': savedMovie.releaseDate,
         'timestamp': DateTime.now().millisecondsSinceEpoch
       });
     }
@@ -35,16 +81,96 @@ class FirestoreService{
     }
   }
 
-  Future<List<QueryDocumentSnapshot>> getUsersSavedItems() async {
+  Future<void> postBookonFirestore(SavedBook savedBook) async {
     await _auth.checkInternConnection();
     final _email = await getCurrentUserEmail();
 
-    QuerySnapshot querySnapshot = await _firestore
-        .collection('saved')
-        .where('email', isEqualTo: _email)
-        .orderBy('timestamp', descending: true)
-        .get();
+    DocumentReference _ref =
+    _firestore.collection('books').doc();
 
-    return querySnapshot.docs;
+    try{
+      await _ref.set({
+        'email': _email,
+        'title': savedBook.title,
+        'lang': savedBook.language,
+        'author': savedBook.author,
+        'genre1': savedBook.genre1,
+        'genre2': savedBook.genre2,
+        'relDate': savedBook.releaseDate,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      });
+    }
+    catch (err) {
+      throw Exception(err);
+    }
+  }
+
+  Future<void> postGameonFirestore(SavedGame savedGame) async {
+    await _auth.checkInternConnection();
+    final _email = await getCurrentUserEmail();
+
+    DocumentReference _ref =
+    _firestore.collection('games').doc();
+
+    try{
+      await _ref.set({
+        'email': _email,
+        'title': savedGame.title,
+        'lang': savedGame.language,
+        'genre1': savedGame.genre1,
+        'genre2': savedGame.genre2,
+        'relDate': savedGame.releaseDate,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      });
+    }
+    catch (err) {
+      throw Exception(err);
+    }
+  }
+
+  Future<void> postTVShowOnFirestore(SavedTVShow savedTvShow) async {
+    await _auth.checkInternConnection();
+    final _email = await getCurrentUserEmail();
+
+    DocumentReference _ref =
+    _firestore.collection('tv_shows').doc();
+
+    try{
+      await _ref.set({
+        'email': _email,
+        'title': savedTvShow.title,
+        'lang': savedTvShow.language,
+        'genre1': savedTvShow.genre1,
+        'genre2': savedTvShow.genre2,
+        'relDate': savedTvShow.releaseDate,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      });
+    }
+    catch (err) {
+      throw Exception(err);
+    }
+  }
+
+  Future<void> postPodcastonFirestore(SavedPodcast savedPodcast) async {
+    await _auth.checkInternConnection();
+    final _email = await getCurrentUserEmail();
+
+    DocumentReference _ref =
+    _firestore.collection('podcasts').doc();
+
+    try{
+      await _ref.set({
+        'email': _email,
+        'title': savedPodcast.title,
+        'lang': savedPodcast.language,
+        'genre1': savedPodcast.genre1,
+        'genre2': savedPodcast.genre2,
+        'relDate': savedPodcast.releaseDate,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      });
+    }
+    catch (err) {
+      throw Exception(err);
+    }
   }
 }
