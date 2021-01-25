d1253877e848f886c2c01b70705866140a4746b3
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2021-01-25 17:53:30 +0500

    Routes and bloc providers added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,12 @@
 import 'package:butler_app/src/bloc/auth_bloc.dart';
+import 'package:butler_app/src/bloc/library_bloc.dart';
+import 'package:butler_app/src/bloc/menu_bloc.dart';
 import 'package:butler_app/src/resources/auth_repository.dart';
+import 'package:butler_app/src/resources/library_repository.dart';
 import 'package:butler_app/src/ui/screens/auth_screen.dart';
 import 'package:butler_app/src/ui/screens/dashboard_screen.dart';
 import 'package:butler_app/src/ui/screens/landing_screen.dart';
+import 'package:butler_app/src/ui/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +17,13 @@ class ButlerApp extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => AuthBloc(AuthRepository()),
-        )
+        ),
+        BlocProvider(
+          create: (context) => MenuBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LibraryBloc(LibraryRepository()),
+        ),
       ],
       child: MaterialApp(
         initialRoute: LandingScreen.id,
@@ -21,6 +31,7 @@ class ButlerApp extends StatelessWidget {
           LandingScreen.id: (context) => LandingScreen(),
           AuthScreen.id: (context) => AuthScreen(),
           DashboardScreen.id: (context) => DashboardScreen(),
+          SearchScreen.id: (context) => SearchScreen(),
         },
       ),
     );
